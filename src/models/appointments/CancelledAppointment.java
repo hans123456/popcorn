d8024ae692d533dd189103d63eeb181822504ae6
Gege <cugeraldine@yahoo.com>
2015-04-19 19:51:23 +0800

    d ko pala napush ito hahaha sorry

---
+++ src/models/appointments/CancelledAppointment.java
@@ -0,0 +1,81 @@
+package models.appointments;
+
+public class CancelledAppointment {
+
+	private int id;
+	private String doctor_name;
+	private String appointee_name;
+	private String date;
+	private String time;
+	private int doctor_id;
+	private int user_id;
+	private int time_id;
+	private String reason;
+	
+	public CancelledAppointment(int id, int doctor_id, String doctor_name, String appointee_name, String date, String time, String reason) {
+		super();
+		this.id = id;
+		this.doctor_id = doctor_id;
+		this.doctor_name = doctor_name;
+		this.appointee_name = appointee_name;
+		this.date = date;
+		this.time = time;
+		this.reason = reason;
+	}
+	
+	public CancelledAppointment(int id, int doctor_id, int user_id, String date, int time_id) {
+		super();
+		this.id = id;
+		this.doctor_id = doctor_id;
+		this.user_id = user_id;
+		this.date = date;
+		this.time_id = time_id;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public String getDoctorName() {
+		return doctor_name;
+	}
+
+	public String getAppointeeName() {
+		return appointee_name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public int getDoctorId() {
+		return doctor_id;
+	}
+
+	public int getUserId() {
+		return user_id;
+	}
+
+	public int getTimeId() {
+		return time_id;
+	}
+	
+	public void setDoctorName(String doctor_name) {
+		this.doctor_name = doctor_name;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	
+	
+}
---
+++ src/tables/Cancelled_appointments_Table.java
@@ -0,0 +1,10 @@
+package tables;
+
+public final class Cancelled_appointments_Table {
+	
+	public final String TABLE_NAME = "`cancelled appointments`";
+	public final String ID = "`cancelled appointments`.id";
+	public final String APPOINTMENT_ID = "`cancelled appointments`.appointment_id";
+	public final String REASON = "`cancelled appointments`.reason";
+
+}
